d48cd4df8a4e808cf0c8829d8a3c889f2903b612
chengdh <cheng.donghui@gmail.com>
2013-06-23 16:11:06 +0800

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'custom stock',
+    'version': '0.1',
+    'category': 'Stock Management',
+    'description': """custome stock module""",
+    'author': 'chengdh (cheng.donghui@gmail.com)',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': ['stock', 'web'],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'active': False,
+    'installable': True,
+    'web':True,
+    'css': [
+    ],
+    'js': [
+    ],
+    'xml': [
+    ],
+}
+
---
+++ report/picking.mako
@@ -0,0 +1,26 @@
+#coding: utf-8
+# vim: set ft=html:
+<html>
+<body>
+<h3>测试入库单</h3>
+<table>
+<tr>
+<td>入库时间</td>
+<td></td>
+<td>仓库</td>
+<td></td>
+</tr>
+<tr>
+<td colspan='4'></td>
+</tr>
+<tr>
+<td colspan='4'></td>
+</tr>
+<tr>
+<td colspan='4'></td>
+</tr>
+</table>
+</body>
+</html>
+
+# vim: set filetype=html:
